678b7d4d634a98d114aa1ef65db18daca0b98f30
John Liu <Liudesign@gmail.com>
2016-10-23 16:05:41 -0400

    changes to app.js, package.json

--- src/app.js
+++ src/app.js
@@ -2,17 +2,25 @@
 
 var express = require('express'),
 	posts = require('./mock/posts.json');
+	
 var app = express();
 
 app.get('/', function(req, res){
-	res.send("<h1>I Love Treehouse.</h1>");
+	res.send("<h1>I am loving Treehouse!</h1>");
 });
 
-app.get('/blog', function (req, res) {
-	res.send(posts);
+app.get('/blog/:title?', function(req, res){
+	var title = req.params.title;
+	if (title === undefined){
+		res.send("This page is under construction!")
+	}else{
+		var post = posts[title];
+		res.send(post);
+	}
+	
 })
 
-app.listen(3000, function () {
+app.listen(3000, function(){
 	console.log("The frontend server is running on port 3000!")
 });
 
