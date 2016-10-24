21ca5754853967ed77b869e3ccfc11e80d5970b1
John Liu <Liudesign@gmail.com>
2016-10-23 20:23:55 -0400

    add files, changes

--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
 'use strict';
 
 var express = require('express'),
-	posts = require('./mock/posts.json');
-	
+	  posts = require('./mock/posts.json');
+
+var postsLists = Object.keys(posts).map(function(value) {
+							         return posts[value]})
+
 var app = express();
 
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/templates');
+
 app.get('/', function(req, res){
-	res.send("<h1>I am loving Treehouse!</h1>");
+	res.render('index');
 });
 
-app.get('/blog/:title?', function(req, res){
+app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
-	if (title === undefined){
-		res.send("This page is under construction!")
-	}else{
-		var post = posts[title];
-		res.send(post);
+	if (title === undefined) {
+		res.status(503);
+		res.render('blog', {posts: postsLists})
+	} else {
+		var post = posts[title] || {};
+		res.render('post', { post: post});
 	}
-	
+});
+
+app.get('/posts', function(req, res){
+	if (req.query.raw){
+		res.json(posts);
+	}else{
+		res.json(postsLists);
+	}
 })
 
-app.listen(3000, function(){
-	console.log("The frontend server is running on port 3000!")
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
 });
-
